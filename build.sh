3ad82920090b2d2dadc11e78c417301f428a2e50
paantya <paantya@gmail.com>
2022-12-17 22:59:11 +0300

    [add] build sh file

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker run  --restart always -v $(pwd):$(pwd) --name $(basename $(pwd)) python:3.6-slim /bin/sh -c "/usr/local/bin/python -m pip install --upgrade pip; cd $(pwd); pip3 install -r ./requirements.txt; pip3 cache purge; python3 ./run.py 2> w.err"
